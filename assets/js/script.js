00032e8293ef278bc627fd462ee8a8bb93ba9a6b
KJ <kj3641402@gmail.com>
2021-05-14 01:12:43 -0400

    added previous search to side and bottom menus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
-let searchEl = $('.search');
+
 let cityInputEl = $('.input');
-let stateInputEl = $('#state');
 let resultContainer = $('.results');
-let cityEl = $('#oldCities')
+let container =$('.resultsAndHistory')
+let cityEl = $('.oldCities')
 let city;
 let cities=[];
 let state;
 let states=[]
+let cityState=[];
 let selectedState
 let empty=[];
 let allBrew
 let brewLat;
 let brewLong;
-let unique
-let brewName
+let uniqueCities=[];
+let uniqueStates=[];
+let brewName;
 let lat; 
 let long;
-let index;
 let lessBtn;
 let moreBtn;
 let nearBtn;
@@ -29,23 +30,41 @@ let renderCities = function () {
   cityEl.children().remove()
   
   let remember = JSON.parse(localStorage.getItem("cities"))
- console.log(remember)
+
    if (remember) {
-     $('<option value="">Select City</option>').appendTo(cityEl);
+     $('<option value="">Select</option>').appendTo(cityEl);
      cities.unshift(remember)
-     list = cities.toString().split(",");
-     unique = [...new Set(list)];
-   for (let i=0; i < unique.length; i++) {
-     $('<option value ='+ unique[i] +' id=' + i + '>'+ unique[i] +'</option>').appendTo(cityEl);
+     list = remember.toString().split(",");
+     uniqueCities = [...new Set(list)];
+    // console.log(cities)
+    // console.log(list)
+    // console.log(uniqueCities)
+   for (let i=0; i < uniqueCities.length; i++) {
+     $('<option value ='+ uniqueCities[i] +' id=' + i + '>'+ uniqueCities[i] +'</option>').appendTo(cityEl);
  }}else {
-   $('#oldCities').hide()
-   unique=cities
+   $('.oldCities').hide()
+   uniqueCities=cities
   
  }
  }
-
  renderCities();
 
+// renders user's recent searches to .history
+
+function renderHistory() {
+  for (let i=0; i<historyStored.length; i++) {
+   
+    history.append('<div class="result-div ml-3">' + historyStored[i] + '</div>');
+    history.children($('.result-div')).children($('.foot-div')).children(".save-button").remove();
+    ($('.card-header')).siblings().css('display', 'none')
+    moreBtn = $('<button class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down icon" aria-hidden="true"></i></span></button>')
+    $('.lessBtn').replaceWith(moreBtn)
+    $('.history .lessDiv').addClass('moreDiv')
+    $('.history .lessDiv').toggleClass('lessDiv')
+
+  };
+};
+renderHistory();
  
 // Collect city and state information from form submission
 
@@ -53,6 +72,7 @@ let formSubmitHandler = function () {
  
     if (city) {
       city = city.replaceAll(" ", "%20");
+      
       getBreweries()
       getRestrooms();
       //reset for new search    
@@ -63,48 +83,39 @@ let formSubmitHandler = function () {
       $("#figure").height(400)
       
     } else {
+ //modal for city and state entry
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'center',
+          showConfirmButton: false,
+          timer: 2000,
+        })
+        Toast.fire({
+          icon: 'error',
+          title: 'Please enter a city or a brewery name!'
+        })
 
-      //modal for city and state entry
-      $(".selectCityState").addClass("is-active")
-      $(".close").click(function() {
-         $(".selectCityState").removeClass("is-active");
-      });
     }
+    
 }
 
-$('#oldCities').change(function() {
-  city = $('#oldCities option:selected').text(); 
+$('.oldCities').change(function() {
+  city = $('.oldCities option:selected').text(); 
   formSubmitHandler()
 })
 
-$(".searchBtn").click(function() {
+$(".search").click(function() {
   city = cityInputEl.val();
- console.log(cities)
-  if (unique.includes(city)) {
+  city.toUpperCase()
+  if (uniqueCities.includes(city)) {
   }else {
-    console.log(cities)
     cities.unshift(city)
-    console.log(cities)
-    localStorage.setItem("cities", JSON.stringify(cities));
-    renderCities
+   
   }
 
   formSubmitHandler()
 })
 
-let stateModal = function() {
- // console.log("modal")
-  $(".stateSelect").addClass("is-active")
- $('#stateModal').change(function() {
-  selectedState = $('#stateModal option:selected').text(); 
- // console.log(selectedState)
-  displayBreweries()
-  $('#stateModal').children().remove()
-  $(".stateSelect").removeClass("is-active") 
-
-  })
-
-}
 
 // Fetch lists of safe unisex restrooms using city and "Brew". 
 let getRestrooms = function () {
@@ -118,7 +129,7 @@ let getRestrooms = function () {
               for(let k = 0; k < allBrew.length; k++) {
                 if ((allBrew[k].street == answer[i].street) || (allBrew[k].name == answer[i].name)) {
                   
-                  let neutral = $('<div class=" mt-4 safe-div"></div>').text("This brewery has a gender-neutral restroom! 👍");
+                  let neutral = $('<div class=" mt-4 safe-div"></div>').text("This brewery has a gender-neutral restroom!");
                   $('#result' + k).children('.safe-div').remove();
                   $('#result' + k).append(neutral)
                   // this removes the button a user can click to show nearest gender-neutral bathroom
@@ -130,48 +141,67 @@ let getRestrooms = function () {
 // fetch breweries by city and then ensure correct state.  
 
 let getBreweries = function () {
- 
     let brewUrl = 'https://api.openbrewerydb.org/breweries/search?query=' + city +''
-   //   console.log(city)
+  
       fetch(brewUrl)
         .then(response => response.json())
         .then(function(response) {
           // in cases where there's no response
           if (response.length == 0) {
-            $(".noResponse").addClass("is-active")
-            $(".close").click(function() {
-              $(".noResponse").removeClass("is-active");
+            cities.shift()
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'center',
+              showConfirmButton: false,
+              timer: 2000,
+            })
+            Toast.fire({
+              icon: 'error',
+              title: 'Oops! No results found.  Please check your spelling and try again!'
+            }).then(() => {
               location.reload();
-            });
+            })
           }else {
+            localStorage.setItem("cities", JSON.stringify(cities));
+            renderCities()
 
-          for (let i=0; i<response.length; i++) {
-           states.push(response[i].state)
-        } 
-          list = states.toString().split(",");
-          unique = [...new Set(list)];
-        if (unique.length > 1) {
-         let noOpt = $('<option value="">Select a State</option>')
-          $('#stateModal').append(noOpt)
-          for (let i=0; i<unique.length; i++) {
-          let stateOpt= $('<option value='+unique[i]+'>'+ unique[i]+'</option>')
-            $('#stateModal').append(stateOpt)
-            
-          }
-          allBrew = response.concat(empty)
-        //   console.log(allBrew)
-           stateModal()
-        //   console.log(selectedState)
-        }else {
-           allBrew = response.concat(empty)
-           selectedState=unique
-          displayBreweries();
-        }
-      } 
-    }) 
-  }
+            allBrew = response.concat(empty)
+     
+            for (let i=0; i<response.length; i++) {
+             states.push(response[i].state)
+             } 
+    
+              list = states.toString().split(",");
+              uniqueStates= [...new Set(list)];
+                if (uniqueStates.length > 1) {
+                  for (let i=0; i<uniqueStates.length; i++) {
+                    cityState.push(uniqueStates[i])
+                  }
+                  Swal.fire({
+                    position: 'center',
+                    confirmButtonColor: 'rgb(126, 163, 145)',
+                    title: 'Select state',
+                    input: 'select',
+                    inputOptions: {
+                    'State': cityState
+                    },
+                    showCancelButton: true,
+                  }).then(response => {
+                      selectedState = cityState[response.value]
+                      displayBreweries();
+                      states = []
+                      cityState = []
+                      console.log(uniqueStates)
+                  })    
+                }else {
+                  selectedState = uniqueStates
+                  displayBreweries();
+                }
+          } 
+        }) 
+}
         let displayBreweries = function () {       
-          
+          $('#searched').css('display', '')
          //  console.log(allBrew)
          //   console.log(selectedState)
 
@@ -184,34 +214,28 @@ let getBreweries = function () {
               //Dynamically create divs for Brewery information
               
 
-              let resultDiv = $('<div class="card result-div" ></div>');
-               headDiv = $('<header id="'+i+'"class="card-header"></header>')
-              let contentDiv = $('<div id="result' + i + '" class=" ml-4 content content'+i+'"></div)')
-              let footDiv = $('<footer class="card-footer foot-div"></footer>')
-               // moreBtn = $('<button id='+i+' class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span></button>')
-                brewName = $('<p class="card-header-title brewName">'+ allBrew[i].name + '</p>')  
-              let saveBtn = $('<button style="width:20%" class="save-button">Save It</button>');
-              let brewStreet = $('<div "class="brewStreet pt-2">').text(allBrew[i].street)
-              let brewAdd =$('<div class="brewAdd pb-2 is-capitalized">').text(city + ', ' + selectedState);
-              let brewWeb = $('<a target="_blank"  class="brewWeb" href='+ allBrew[i].website_url +'>Click to visit website</a><br>');
-                  nearBtn = $('<button  class="mt-3 has-text-centered nearBtn">Find nearest gender-neutral restroom!</button><br>');
-              lat = $('<span class="lt">'+ allBrew[i].latitude +'</span>')
-              long = $('<span class="lng">'+ allBrew[i].longitude +'</span>')
+              let resultDiv = $('<div class="result-div ml-3"></div>');
+                  headDiv = $('<header id="'+i+'"class="card-header moreDiv"></header>')
+              let contentDiv = $('<div style="display: none" id="result' + i + '" class="content content'+i+'"></div)')
+              let footDiv = $('<footer style="display: none" class="card-footer foot-div"></footer>')
+                  moreBtn = $('<button class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down icon" aria-hidden="true"></i></span></button>')
+                  brewName = $('<b class="card-header-title brewName">'+ allBrew[i].name + '</b>')  
+              let saveBtn = $('<button style="width:20%" class="save-button button  '+ allBrew[i].name +'">Save</button>');
+              let brewStreet = $('<div "class="brewStreet">').text(allBrew[i].street)
+              let brewAdd =$('<div class="brewAdd  pb-2 is-capitalized">').text(city + ', ' + selectedState);
+              let brewWeb = $('<a target="_blank"  class="brewWeb " href='+ allBrew[i].website_url +'>Click to visit website</a><br>');
+                  nearBtn = $('<button  class="mt-3 button has-text-centered nearBtn">find nearest neutral restroom!</button><br>');
+                  lat = $('<span class="lt">'+ allBrew[i].latitude +'</span>')
+                  long = $('<span class="lng">'+ allBrew[i].longitude +'</span>')
               resultContainer.append(resultDiv);
               resultDiv.append(headDiv, contentDiv, footDiv)
-              headDiv.append(brewName)
+              headDiv.append(brewName, moreBtn)
               footDiv.append(saveBtn)
               if (allBrew[i].website_url == "") {
                 contentDiv.append(brewStreet, brewAdd, nearBtn)
               } else {
                 contentDiv.append(brewStreet, brewAdd, brewWeb, nearBtn,)
               };
-
-              // fixes issue with save button styling on mobile viewports
-              let mobileSize = window.matchMedia('(max-width: 500px)');
-              if (mobileSize.matches) {
-                $('.save-button').removeClass('is-pulled-right')
-              }
 
               nearBtn.append(lat, long)
               $('.lt, .lng').hide();
@@ -227,45 +251,33 @@ let getBreweries = function () {
                   nearBtn.remove()
                 }
               }
-            }
+             }
             }    
           renderHistory();
       }
 
-
-resultContainer.on("click", ".moreBtn", function(event) {
-  event.stopPropagation();
-  
-  console.log($(this.parentElement.siblingElement))
-  lessBtn = $('<button class="card-header-icon lessBtn" aria-label="less options"><span class="icon"><i class="fa fa-chevron-up" aria-hidden="true"></i></span></button>')
-  $(this).replaceWith(lessBtn)
-})
-
-resultContainer.on("click", ".lessBtn", function() {
-   
-  $(this).parent(brewName).children().css('display', 'none')
-  moreBtn = $('<button class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span></button>')
-  $(this).replaceWith(moreBtn)
-})
-
-history.on("click", ".moreBtn", function() {
-  
-  $(this).siblings(brewName).children().css('display', '')
-  lessBtn = $('<button style="width:98%" class="icon-button lessBtn"><i class="fa fa-chevron-up is-pulled-right" style="font-size:16px"></i></button>')
-  $(this).replaceWith(lessBtn)
+container.on("click", '.moreDiv', function() {
+  $(this).addClass('lessDiv')
+  $(this).toggleClass('moreDiv')
+  $(this).siblings().css('display', '')
+  lessBtn = $('<button class="card-header-icon lessBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-up icon" aria-hidden="true"></i></span></button>')
+  $(this).children().last().replaceWith(lessBtn)
+ 
 })
  
-history.on("click", ".lessBtn", function() { 
-  $(this).siblings(brewName).children().css('display', 'none')
-  moreBtn = $('<button style="width:98%" class="icon-button moreBtn"><i class="fa fa-chevron-down is-pulled-right" style="font-size:16px"></i></button>')
-  $(this).replaceWith(moreBtn)
+container.on("click", ".lessDiv", function() { 
+  $(this).addClass('moreDiv')
+  $(this).toggleClass('lessDiv')
+  $(this).siblings().css('display', 'none')
+  moreBtn = $('<button class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down icon" aria-hidden="true"></i></span></button>')
+  $(this).children().last().replaceWith(moreBtn)
 })
 
 // this will be used to line up the rendering in nearestRestroom() with the button that was clicked 
     let classCounter = 0
 
 // event handler for fetching nearest gender-neutral bathroom
-resultContainer.on("click", ".nearBtn", function() {
+container.on("click", ".nearBtn", function() {
   brewLat =  $(this).children().first().text();
   brewLong =  $(this).children().last().text();
  
@@ -276,16 +288,6 @@ resultContainer.on("click", ".nearBtn", function() {
     nearestRestroom();
  
 });
-
-// event handler for fetching nearest gender-neutral bathrom when user clicks from history 
-  history.on("click", ".nearBtn", function() {
-    brewLat =  $(this).children().first().text();
-    brewLong =  $(this).children().last().text();
-    // console.log(brewLong)
-    classCounter++
-    $(this).addClass("btn" + classCounter)
-    nearestRestroom();
-})
 
 // function for fetching nearest gender-neutral bathroom info 
 function nearestRestroom() {
@@ -298,10 +300,10 @@ function nearestRestroom() {
 
           if (stuff[0] !== undefined) {
             let nearestTitle = $('<div class="mt-6  nearestTitle"></div>').text('Nearest Gender-Neutral Restroom:');
-            let nearestName = $('<div class="mb-1   subtitle nearestName"></div>').text(stuff[0].name);
-            let nearestStreet = $('<div class="nearestStreet mx-1 p-1  nearestStreet"></div>').text(stuff[0].street);
-            let nearestAddress  =$('<div class="mx-1 p-1   is-capitalized nearestAddress"></div>').text(stuff[0].city + ', '+ stuff[0].state)
-            // console.log( $("#result" + index))
+            let nearestName = $('<div class="mb-1 nearestName"></div>').text(stuff[0].name);
+            let nearestStreet = $('<div class="nearestStreet   nearestStreet"></div>').text(stuff[0].street);
+            let nearestAddress  =$('<div class="is-capitalized nearestAddress"></div>').text(stuff[0].city + ', '+ stuff[0].state)
+            // console.log( $("#result))
 
             $(".btn" + classCounter).after(nearestTitle, nearestName, nearestStreet, nearestAddress);
             $(".btn" + classCounter).remove();
@@ -314,37 +316,33 @@ function nearestRestroom() {
 };
 // delegated event handler for saving brewery/bathroom info 
 resultContainer.on("click", ".save-button", function() {
-  $(this).text("Saved ✅")
+  $(this).text("Saved")
+ 
   let thisBrew = $(this).parent().parent().html();
-   console.log(thisBrew)
-  console.log(historyStored)
+
   if (historyStored.includes(thisBrew)) {
 
   } else if (thisBrew == undefined || thisBrew == null) {
 
   } else {
      
-      // console.log(historyStored)
       historyStored.push(thisBrew);
       localStorage.setItem("history-info", JSON.stringify(historyStored));
-      history.append('<div class="result-div p-3 mr-6 ">' + thisBrew + '</div>');
+      history.append('<div class="result-div ml-3">' + thisBrew + '</div>');
       history.children($('.result-div')).children($('.foot-div')).children(".save-button").remove();
+      ($('.history .card-header')).siblings().css('display', 'none')
+      moreBtn = $('<button class="card-header-icon moreBtn" aria-label="more options"><span class="icon"><i class="fa fa-chevron-down icon" aria-hidden="true"></i></span></button>')
+      $('.history .lessBtn').replaceWith(moreBtn)
+      $('.history .lessDiv').addClass('moreDiv')
+      $('.history .lessDiv').toggleClass('lessDiv')
+ 
   };
 
 });
 
 
 
-// renders user's recent searches to .history
-function renderHistory() {
-  for (let i=0; i<historyStored.length; i++) {
-    history.append('<div class="result-div  mr-6">' + historyStored[i] + '</div>');
-    let historyChildren = $('.history .foot-div .save-button');
-    historyChildren.remove();
 
-  };
-};
-renderHistory();
 
 // event handler for deleting history
 $(".resultsAndHistory").on("click", ".delete-btn", function() {
@@ -354,8 +352,22 @@ $(".resultsAndHistory").on("click", ".delete-btn", function() {
   renderHistory=[]
 })
 
-// media queries 
-let mobileSize = window.matchMedia('(max-width: 500px)');
-if (mobileSize.matches) {
-  
+$('nav').on("click", "a", function() {
+ 
+  $('.navbar-burger').toggleClass('is-active');
+  $('.butts').toggleClass('is-active');
+})
+
+$('nav').on("click", ".navbar-link", function() {
+  $('.oldTowns').children().remove()
+for (let i=0; i < uniqueCities.length; i++) {
+  $('<a class="navbar-item town" value ='+ uniqueCities[i] +' id=' + i + '>'+ uniqueCities[i] +'</a>').appendTo($('.oldTowns'))
 }
+ $('.navbar-dropdown').toggleClass('is-hidden-touch');
+})
+
+$('.oldTowns').on("click", ".town", function() {
+  $('.navbar-dropdown').toggleClass('is-hidden-touch');
+  city = $(this).text(); 
+  formSubmitHandler()
+})
